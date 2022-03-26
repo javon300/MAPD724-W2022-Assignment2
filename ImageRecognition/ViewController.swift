d8832de4c62a66988fbf8602992dcba45298d877
matthew maxwell <matthewmaxwell300@yahoo.com>
2022-03-26 02:24:06 -0600

    implemented the Image picker
    adds image to display from user device

--- ImageRecognition/ViewController.swift
+++ ImageRecognition/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 import CoreML
 import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     @IBOutlet weak var labelDescription: UILabel!
     
+    @IBOutlet weak var habitatBtn: UIButton!
+    @IBOutlet weak var camaraBtn: UIButton!
+    @IBOutlet weak var photosBtn: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
@@ -45,6 +48,40 @@ class ViewController: UIViewController {
           }
        }
        labelDescription.text = "Image is: \(bestGuess) with confidence \(bestConfidence) out of 1"
+    }
+    
+    
+    /********************************************************       BUTTON PRESSED FUNCTIONS   ****************************************************/
+    //calls image picker and sets a new image
+    @IBAction func PhotosBtnPressed(_ sender: Any)
+    {
+        let imagePickerVC = UIImagePickerController()
+        imagePickerVC.sourceType = .photoLibrary
+        imagePickerVC.delegate = self // new
+        present(imagePickerVC, animated: true)
+    }
+    
+    @IBAction func camaraPressed(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func habitatBtnPressed(_ sender: Any)
+    {
+        
+    }
+    
+    
+    /********************************************************       BUTTON PRESSED FUNCTIONS  END    ****************************************************/
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        picker.dismiss(animated: true, completion: nil)
+
+        if let image = info[.originalImage] as? UIImage
+        {
+            imageView.image = image
+        }
     }
 
 

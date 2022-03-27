b00e9793f37f15c0a80e66bb0e7e9b1d87a6eef6
matthew maxwell <matthewmaxwell300@yahoo.com>
2022-03-27 00:18:11 -0600

    labels on map screen displays info about animal uploaded

--- ImageRecognition/habitatViewController.swift
+++ ImageRecognition/habitatViewController.swift
@@ -13,7 +13,12 @@ class habitatViewController: UIViewController {
     @IBOutlet weak var detailLabel: UILabel!
     @IBOutlet weak var animalTypeLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
+    
+    //value set by segue in view controller
     var passedDetails: String = ""
+    
+    var animalType: String = ""
+    var details: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +28,10 @@ class habitatViewController: UIViewController {
         let location = CLLocationCoordinate2D(latitude: 51.50007773,longitude: -0.1246402)
         let querry = "canada"
         
-        
+        //checks for animal name in string
+        checkForAnimal(name: passedDetails)
         
        getLocation(query: querry,locat_place: location)
-        detailLabel.text = passedDetails
     }
     func getLocation(query:String, locat_place: CLLocationCoordinate2D)
     {
@@ -42,49 +47,66 @@ class habitatViewController: UIViewController {
         mapView.addAnnotation(annotation)
     }
     
-    func animalCheck(animal: String)
+    func checkForAnimal(name: String)
     {
-        if (animal == "cat")
+        if (name.contains("cat"))
         {
-        
+            animalType = "cat"
+            animalTypeLabel.text = animalType
+            let about = "the household cat. Probably found in your house if not, its in your neighbour's house"
+            detailLabel.text = about
         }
-        if (animal == "dog")
+        else if (name.contains("dog"))
         {
-            
+            animalType = "dog"
+            animalTypeLabel.text = animalType
+            let about = "the household dog. Probably found in your house if not, its in your neighbour's house"
+            detailLabel.text = about
         }
-        if (animal == "lion")
+        else if (name.contains("lion"))
         {
-            
+            animalType = "lion"
+            animalTypeLabel.text = animalType
+            let about = "Most commonly found in African jungles. They are present in other regions also such as australia"
+            detailLabel.text = about
         }
-        if (animal == "tiger")
+        else if (name.contains("tiger"))
         {
-            
+            animalType = "tiger"
+            animalTypeLabel.text = animalType
+            let about = "Most commonly found in African jungles. They are present in other regions also such as australia"
+            detailLabel.text = about
         }
-        if (animal == "aligator")
+        else if (name.contains("aligator"))
         {
-            
+            animalType = "aligator"
+            animalTypeLabel.text = animalType
+            let about = "Most commonly found in African jungles. They are present in other regions also such as australia"
+            detailLabel.text = about
         }
-        if (animal == "crocodile")
+        else if (name.contains("crocodile"))
         {
-            
+            animalType = "crocodile"
+            animalTypeLabel.text = animalType
+            let about = "Most commonly found in African jungles. They are present in other regions also such as australia"
+            detailLabel.text = about
         }
-        if (animal == "kangaroo")
+        else if (name.contains("kangaroo"))
         {
-            
+            animalType = "Kangaroo"
+            animalTypeLabel.text = animalType
+            let about = "Most commonly found in Australia grasslands. They are present in other regions also such as Africa"
+            detailLabel.text = about
+        }
+        else
+        {
+            animalType = "NOT AN ANIMAL"
+            animalTypeLabel.text = animalType
+            let about = "The image you entered is not a registered animal please upload a different picture of an animal"
+            detailLabel.text = about
         }
         
     }
 
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//           Get the new view controller using segue.destination.
-//           Pass the selected object to the new view controller.
-//      }
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-//    {
-//
-//        guard let detailVC = segue.destination as? ViewController else { return }
-//        let details = detailVC.details
-//        detailLabel.text = details
-//    }
+  
 }

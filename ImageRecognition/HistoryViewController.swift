d47f51d99948991b7a3365a1100f2bf4bb02e17d
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2022-03-27 23:34:59 -0400

    Added ui design:

--- ImageRecognition/HistoryViewController.swift
+++ ImageRecognition/HistoryViewController.swift
@@ -1,8 +1,18 @@
-//
-//  HistoryViewController.swift
-//  ImageRecognition
-//
-//  Created by Aishwarya Shrestha on 27/03/2022.
-//
+//Author Name: Aishwarya Shrestha
+//Student ID: 301138662
+//Email ID: ashres15@my.centennialcollege.ca
+
+//Author Name: Keshav Dulal
+//Student ID: 301209947
+//Email ID: kdulal1@my.centennialcollege.ca
+
+//Author Name: Poojan Jitendrakumar Patel
+//Student ID: 301228811
+//Email ID: ppate821@my.centennialcollege.ca
+
+//Author Name: Matthew Maxwell
+//Student ID: 301200258
+//Email ID: mmaxwel7@my.centennialcollege.ca
+
 
 import Foundation
--- ImageRecognition/MapViewController.swift
+++ ImageRecognition/MapViewController.swift
@@ -1,9 +1,19 @@
-////
-////  MapViewController.swift
-////  ImageRecognition
-////
-////  Created by javon maxwell on 2022-03-26.
-////
+//Author Name: Aishwarya Shrestha
+//Student ID: 301138662
+//Email ID: ashres15@my.centennialcollege.ca
+
+//Author Name: Keshav Dulal
+//Student ID: 301209947
+//Email ID: kdulal1@my.centennialcollege.ca
+
+//Author Name: Poojan Jitendrakumar Patel
+//Student ID: 301228811
+//Email ID: ppate821@my.centennialcollege.ca
+
+//Author Name: Matthew Maxwell
+//Student ID: 301200258
+//Email ID: mmaxwel7@my.centennialcollege.ca
+
 //
 //import Foundation
 //

fe450e9ea6cad45d04fbc762ce98026266e00fe0
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2022-04-18 09:06:55 -0400

    added ui design

--- ImageRecognition/HelpViewController.swift
+++ ImageRecognition/HelpViewController.swift
@@ -10,9 +10,74 @@ import UIKit
 class HelpViewController: UIViewController
 {
 
+//    @IBOutlet weak var sliderCollectionView: UICollectionView!
+//    
+//    var imgArr = [UIImage(named: "instruction_1"),
+//                  UIImage(named: "instruction_2")]
+//    
+//    var timer = Timer()
+//    var counter = 0
+//    
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
     }
+    
+    
+//    override func didReceiveMemoryWarning()
+//    {
+//        super.didReceiveMemoryWarning()
+//
+//    }
 }
+
+//
+//extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section:Int) -> Int {
+//        return imgArr.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+//        if let vc = cell.viewWithTag(111) as? UIImageView {
+//            vc.image = imgArr[indexPath.row]
+//        } else if let ab = cell.viewWithTag(222) as? UIPageControl {
+//            ab.currentPage = indexPath.row
+//        }
+//
+//        return cell
+//    }
+//
+//
+//}
+//
+//
+//extension ViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:
+//                        UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//
+//    }
+//
+//
+//   func collectionView(_ collectionView: UICollectionView, layout
+//                                        collectionViewLayout: UICollectionViewLayout, sizeForItemAtsizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let size = sliderCollectionView.frame.size
+//        return CGSize(width: size.width, height: size.height)
+//
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:
+//                        UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0.0
+//
+//    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:
+//                        UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0.0
+//
+//    }
+//}
+//

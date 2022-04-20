d78294946225dc130db57a2e4076e8f2964f3e49
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2022-04-20 11:35:54 -0400

    added theme

--- ImageRecognition/SettingsViewController.swift
+++ ImageRecognition/SettingsViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet var mySwitch: UISwitch!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        overrideUserInterfaceStyle = .dark
+    }
+    
+    
+    
+    @IBAction func switchDidChange(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            view.backgroundColor = .red
+        }
+        else {
+            view.backgroundColor = .blue
+        }
     }
 }
+
